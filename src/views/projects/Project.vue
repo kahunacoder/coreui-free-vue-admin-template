3bfce0a7f3ddee783e3dc78d459434a68d4ff23b
Gary Smith <bk@kc.gs>
2018-08-16 17:31:02 -0400

    Built Project.vue to work with tabs
    Signed-off-by: Gary Smith <bk@kc.gs>

--- src/views/projects/Project.vue
+++ src/views/projects/Project.vue
@@ -1,6 +1,47 @@
 <template>
 <div>
   <div>
+
+      <b-col xs="12" lg="6">
+        <b-card>
+          <div slot="header">
+            Tabs with icons
+          </div>
+          <b-tabs v-model="tabIndex[0]">
+            <b-tab active>
+              <template slot="title">
+                <i class="icon-calculator"></i> {{tabs[0]}}
+              </template>
+              <br> 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </b-tab>
+            <b-tab>
+              <template slot="title">
+                <i class="icon-basket-loaded"></i> {{tabs[1]}}
+              </template>
+              <br>2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </b-tab>
+            <b-tab>
+              <template slot="title">
+                <i class="icon-pie-chart"></i> {{tabs[2]}}
+              </template>
+              <br>3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </b-tab>
+          </b-tabs>
+        </b-card>
+      </b-col>
+
   <b-row>
     <b-col cols="12" lg="6">
       <b-card no-header>
@@ -21,7 +62,7 @@
   </div>
   <div class="card">
     <div class="card-header">
-      Headings
+      Headings {{ project.name }}
     </div>
     <div class="card-body">
       <p>Documentation and examples for Bootstrap typography, including global settings, headings, body text, lists, and more.</p>
@@ -42,11 +83,19 @@ export default {
   },
   data: () => {
     return {
-      items: (slug) => {
+      tabIndex: [0, 0],
+      tabs: [
+        'Calculator',
+        'Shopping cart',
+        'Charts'
+      ],
+      items: function (slug) {
         const project = config.projects.find( project => project.slug === slug)
+        this.project = project
         const projectDetails = project ? Object.entries(project) : [['slug', 'Not found']]
         return projectDetails.map(([key, value]) => {return {key: key, value: value}})
       },
+      project: '',
       fields: [
         {key: 'key'},
         {key: 'value'},
